7211df790d19d551303696e391f98600002e2f78
Jayson Delos santos <98378694+JaysonDelos018@users.noreply.github.com>
2022-05-09 09:53:51 +0800

    Add files via upload

---
+++ Exercise15.js
@@ -0,0 +1,5 @@
+document.getElementById("1").innerHTML="Student Information Sheet";
+document.getElementById("2").innerHTML="Personal Info";
+document.getElementById("3").innerHTML="Contact Information";
+document.getElementById("4").innerHTML="Academic Information";
+
